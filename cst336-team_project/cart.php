2fd256bfb37fe69e6ec156e4c3fe6dc789bdf3d5
Jenn Schopfer <jschopfer@csumb.edu>
2017-03-29 05:14:37 +0000

    updated cart page

--- cst336-team_project/cart.php
+++ cst336-team_project/cart.php
@@ -37,7 +37,7 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="#">Flower Shop</a>
+                <a class="navbar-brand" href="index.php">Flower Shop</a>
             </div>
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
