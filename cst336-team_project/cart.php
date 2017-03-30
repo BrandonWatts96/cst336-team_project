3bdc080e26c76abb8198395419520d654b15a8b3
Jenn Schopfer <jschopfer@csumb.edu>
2017-03-30 06:46:34 +0000

    cart page update

--- cst336-team_project/cart.php
+++ cst336-team_project/cart.php
@@ -1,4 +1,5 @@
 <?php
+function showCart(){
 // start session
 session_start();
  
@@ -36,7 +37,6 @@ if(count($_SESSION['cart'])>0){
 	
     $sql = $sql . $ids[sizeof($ids) -1] .") ORDER BY name";
 
-		echo $sql;
     // prepare query statement
     $stmt = $dbConn->prepare($sql);
 
@@ -97,6 +97,113 @@ else{
                 echo "</a>";
     echo "</div>";
 }
-
+}
  
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Flower Shop</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/heroic-features.css" rel="stylesheet">
+
+
+    <!-- Custom CSS -->
+    <link href="css/styles.css" rel="stylesheet">
+
+
+</head>
+
+<body>
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="index.php">Flower Shop</a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li>
+                        <!----<a href="#">About</a>---->
+                    </li>
+                    <li>
+                        <a href="cart.php">Cart</a>
+                    </li>
+                    <li>
+                        <!----<a href="account.php">Account</a>----->
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+
+    <!-- Page Content -->
+    <div class="container">
+
+        <!-- Jumbotron Header -->
+        <header class="jumbotron hero-spacer">
+            <h1>Flower Shop</h1>
+            <p>Welcome to the CSUMB Flower Shop created and run by students for students.</p>
+        </header>
+
+        <hr>
+
+        <!-- Title -->
+        <div class="row">
+            <div class="col-lg-12">
+                <center><h1>Our Products</h1></center>
+            </div>
+        </div>
+        <!-- /.row -->
+
+        <!-- Page Features -->
+      <?=showCart()?>
+
+        <hr>
+
+        <!-- Footer -->
+        <footer>
+            <div class="row">
+                <div class="col-lg-12">
+                    <center>
+                        <a href="https://docs.google.com/presentation/u/1/d/1bkYpRsHVhn3ic_CkIcRgds0ttgq45RjL1UwC9RP7fZ8/htmlpresent">Link to User Story</a>
+                        <br>
+                        <p>Copyright &copy; CST 336 - Team Project - Flower Shop</p></center>
+                </div>
+            </div>
+        </footer>
+
+    </div>
+    <!-- /.container -->
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
